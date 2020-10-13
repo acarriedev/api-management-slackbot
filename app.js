3f3fab9e48f6cc8ed7c5ece1170ad93f8a555735
“scrymgeourggit config --global user.email alexcarrie.tidza@gmail.comgit commit --amend --reset-authorgit config --global user.name “scrymgeourg <alexcarrie.tidza@gmail.com>
2020-10-13 11:41:24 +0100

    Adds condition for message being from user.

--- app.js
+++ app.js
@@ -1,58 +1,45 @@
-const SlackBot = require('slackbots');
-const { secretToken } = require("./secretToken")
- 
-// create a bot
+const SlackBot = require("slackbots");
+const { secretToken } = require("./secretToken");
+
+
 const bot = new SlackBot({
-    token: secretToken, // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: 'apim-helper2'
+    token: secretToken,
+    name: "apim-helper2"
 });
+const channelUsers = {};
 
-const channelUsers = {}
-
- 
-bot.on('start', function() {
-
-    const userData = bot.getUsers()._value.members
+bot.on("start", () => {
+    const userData = bot.getUsers()._value.members;
     userData.forEach((member) => {
-        channelUsers[member.id] = {msgCount : 0}
+        channelUsers[member.id] = {msgCount : 0};
     })
 
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    // const params = {
-    //     icon_emoji: ':cat:'
-    // };
-    
-    // // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    // bot.postMessageToChannel('general', 'meow!', params);
-    
-    // // define existing username instead of 'user_name'
-    // bot.postMessageToUser('alexander.carrie1', 'meow!', params); 
-    
-    // // If you add a 'slackbot' property, 
-    // // you will post to another user's slackbot channel instead of a direct message
-    // bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-    
-    // // define private group instead of 'private_group', where bot exist
-    // bot.postMessageToGroup('private_group', 'meow!', params); 
+    console.log(channelUsers);
 });
 
-/**
- * @param {object} data 
- */
-bot.on('message', function(data) {
-    const {user, channel, type, ts} = data
+
+bot.on("message", (data) => {
+    const {user, channel, type, ts} = data;
+    const chanTestSpace = channel === "C01CBCG1Z7F";
+    const isUserMessage = user && type === "message";
+
+    // TEST WITH HENRY. COMMENT BACK IN TO SEE IF IT WORKS AFTER USER ADDED
+
+    // if (!(user in channelUsers)) {
+    //     channelUsers[user] = {msgCount : 0}
+    // }
     
-    if (channel === "C01CBCG1Z7F" && type === "message"){
+    if (chanTestSpace && isUserMessage){
         console.log(data)
-        if (channelUsers[user]["msgCount"] == 0) {
-            const confluenceRes = "Hello, thank you for your message. Don't forget to check the API Management Producer support Confluence page at https://nhsd-confluence.digital.nhs.uk/display/APM/API+producer+zone"
+        const chanUser = channelUsers[user];
+
+        if (chanUser && chanUser.msgCount === 0) {
+            const confluenceRes = "Hello, thank you for your message. Don't forget to check the API Management Producer support Confluence page at https://nhsd-confluence.digital.nhs.uk/display/APM/API+producer+zone";
+
             bot.postMessageToChannel('testing-space-for-making-a-slackbot', confluenceRes, {thread_ts: ts});
         }
 
-        channelUsers[user]["msgCount"]++
+        channelUsers[user]["msgCount"]++;
     }
 });
 
-// bot.postMessageToUser('alexander.carrie1', 'howdy').then(function(data) {
-//     console.log(data)
-// })
